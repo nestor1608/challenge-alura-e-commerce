2e0585801f9b8b6d1526f7e9a2e767bf3ee672b3
nestor1608 <nestor.268@Outlook.com>
2022-08-11 23:57:50 -0300

    sever

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ server.use(middlewares);
 server.use(router);
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
-    console.log(`JSON Server is running on port ${port}`);
+    console.log(`listening on *: 3000`);
 });
